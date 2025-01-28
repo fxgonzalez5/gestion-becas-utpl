9716d3aa0a06af4c8426eb5d1ea9e5e1850ea9db
Francisco González <73602789+fxgonzalez5@users.noreply.github.com>
2025-01-28 13:31:43 -0500

    Consumir el servicio para obtener los requisitos de una postulación

--- src/app/panel/pages/requirements-list-page/requirements-list-page.component.ts
+++ src/app/panel/pages/requirements-list-page/requirements-list-page.component.ts
@@ -2,6 +2,8 @@ import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
 import { RequirementCardComponent } from '../../components/requirement-card/requirement-card.component';
+import { AuthService } from '../../../auth/services/auth.service';
+import { ScholarshipsService } from '../../services/scholarships.service';
 import { RequirementsService } from '../../services/requirements.service';
 import { Requirement } from '../../interfaces';
 
@@ -14,6 +16,8 @@ import { Requirement } from '../../interfaces';
 })
 export default class RequirementsListPageComponent implements OnInit {
   private activatedRoute = inject(ActivatedRoute);
+  private authService = inject(AuthService);
+  private scholarshipsService = inject(ScholarshipsService);
   private requirementsService = inject(RequirementsService);
 
   private _requirementsList = signal<Requirement[]>([]);
@@ -21,13 +25,28 @@ export default class RequirementsListPageComponent implements OnInit {
   public requirementsList = computed(() => this._requirementsList());
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(
-      ({ id }) => this.loadRequirements(Number(id) ?? 0)
-    );
+    const userId = this.authService.currentUser()!.id;
+    const applicationId = this.scholarshipsService.appliedScholarshipId();
+    if (applicationId) {
+      this.loadRequirements(0, applicationId, userId);
+    } else {
+      this.activatedRoute.params.subscribe(
+        ({ id }) => this.loadRequirements(Number(id) ?? 0)
+      );
+    }
   }
 
-  private loadRequirements(scholarshipId: number): void {
-    this.requirementsService.getRequirementsByScholarship(scholarshipId)
+  private loadRequirements(scholarshipId: number, applicationId?: number, userId?: string): void {
+    if (applicationId && applicationId > 0 && userId) {
+      this.requirementsService.getRequirementsByApplication(Number(applicationId), userId)
+      .subscribe({
+        next: requirements => this._requirementsList.set(requirements),
+        error: error => console.error(error),
+      });
+      return;
+    }
+
+    this.requirementsService.getRequirementsByScholarship(scholarshipId!)
       .subscribe({
         next: requirements => this._requirementsList.set(requirements),
         error: error => console.error(error),
--- src/app/panel/services/requirements.service.ts
+++ src/app/panel/services/requirements.service.ts
@@ -18,7 +18,19 @@ export class RequirementsService {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.get<RequirementsResponse>(url, { headers })
+    return this.http.get<RequirementsResponse<'scholarship_id'>>(url, { headers })
+      .pipe(
+        map(response => response.requirements),
+        catchError(e => throwError(() => e.error.message))
+      );
+  }
+
+  getRequirementsByApplication(applicationId: number, userId: string): Observable<Requirement[]> {
+    const url = `${this.baseUrl}/application/${applicationId}/requirements/${userId}`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<RequirementsResponse<'application_id'>>(url, { headers })
       .pipe(
         map(response => response.requirements),
         catchError(e => throwError(() => e.error.message))
