4afd5f1516dc6b59cf498b684bc43403fb394949
Francisco González <73602789+fxgonzalez5@users.noreply.github.com>
2025-02-10 21:14:38 -0500

    Eliminación de los borradores creados por Emma

--- src/app/auth/auth.routing.ts
+++ src/app/auth/auth.routing.ts
@@ -1,21 +1,7 @@
 import { Routes } from '@angular/router';
 import LoginPageComponent from './pages/login-page/login-page.component';
-import HomePageComponent from './pages/home-page/home-page.component';
-import ApplicationPageComponent from './pages/grant-pages/application-page/application-page.component';
-import FollowUpPageComponent from './pages/grant-pages/followup-page/followup-page.component';
-import GrantRequirementsPageComponent from './pages/grant-pages/grant-requirements-page/grant-requirements.component';
-import GrantApplicationPageComponent from './pages/grant-pages/grant-application-page/grant-application.component';
-import { LocationPageComponent } from './pages/grant-pages/location-page/location.component';
-import SocioEconomicDataPageComponent from './pages/grant-pages/socio-economic-data-page/socio-economic-data.component';
 
 export const authRoutes: Routes = [
   { path: 'login', component: LoginPageComponent },
-  { path: 'home', component: HomePageComponent },
-  { path: 'application', component: ApplicationPageComponent},
-  { path: 'followup', component: FollowUpPageComponent},
-  { path: 'grantrequirements', component: GrantRequirementsPageComponent},
-  { path: 'grantapplication', component: GrantApplicationPageComponent},
-  { path: 'location', component: LocationPageComponent},
-  { path: 'socioeconomicdata', component: SocioEconomicDataPageComponent},
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
--- src/app/auth/pages/footer-page/footer-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'footer-page',
-  templateUrl: './footer-page.component.html',
-  styleUrls: ['./footer-page.component.css'],
-})
-
-export default class FooterPageComponent {}
